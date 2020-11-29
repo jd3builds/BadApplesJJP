3828038726ddc1df74b56ab5ee1fa0ba65b7a12e
John Dillon <41346111+jd3builds@users.noreply.github.com>
2020-11-29 18:42:05 -0500

    fixed version :P

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setuptools.setup(
     name="BAproducetracker",
-    version="2.1.0",
+    version="2.2.0",
     author="Brody, Joseph // Dillon, John // Estrada, Pablo",
     url="https://github.com/jd3builds/BadApplesJJP",
     packages=['producetracker'],
